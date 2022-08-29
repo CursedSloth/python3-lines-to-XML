4995e14cef8fb0e7af5d38a23ed278f623bce40b
Victor Ingvarsson <victor.ingvarsson@gmail.com>
2022-08-29 17:48:18 +0200

    fix(main): add comments
    Explanatory comments for future reference

--- lines-to-XML.py
+++ lines-to-XML.py
@@ -1,25 +1,40 @@
 import xml.etree.ElementTree as ET
 
-def main(): 
+def main():
+    # open file in read mode
     input_file = open("input.txt", "r")
     input_string = input_file.read()
+
+    # set name for output file
     file_name = "output.xml"
+
+    # Read each line into a list item
     formatted_string = input_string.splitlines()
+
+    # Create base element
     xml = ET.Element("people")
+    
+    # Default state, used to check if inside family for a person
     family_state = False
 
     for items in formatted_string: 
+
+        # Separate string into list by separator
         sep_string = items.split(sep = "|")
 
+        # Person element
         if sep_string[0] == "P":
+            # Set that we are not inside family element
             family_state = False
             person = ET.SubElement(xml, "person") 
             firstname = ET.SubElement(person, "firstname")
             firstname.text = sep_string[1]
             lastname = ET.SubElement(person, "lastname")
             lastname.text = sep_string[2]
-
+        
+        # Telephone element
         elif sep_string[0] == "T":
+            # Check if supposed to be inside family element
             if family_state == True: 
                 phone = ET.SubElement(family, "phone")
             
@@ -31,7 +46,9 @@ def main():
             telephone = ET.SubElement(phone, "telephone")
             telephone.text = sep_string[2]
 
+        # Address element
         elif sep_string[0] == "A": 
+            # Check if supposed to be inside family element
             if family_state == True: 
                 address = ET.SubElement(family, "address")
 
@@ -43,11 +60,14 @@ def main():
             city = ET.SubElement(address, "city")
             city.text = sep_string[2]
             
+            # Check if long enough to include postal number
             if len(sep_string) > 3: 
                 postal_number = ET.SubElement(address, "postalnumber")
                 postal_number.text = sep_string[3]
 
+        # Family element
         elif sep_string[0] == "F":
+            # Set that we are inside family element
             family_state = True
             family = ET.SubElement(person, "family")
             name = ET.SubElement(family, "name")
@@ -61,6 +81,8 @@ def main():
     ET.indent(xml, space="\t", level=0)
 # write to file
     xmlTree.write(file_name, encoding="utf-8")
+
+    input_file.close()
 
 if __name__ == "__main__":
     main()
