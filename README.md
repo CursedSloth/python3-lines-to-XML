0b301eabb138de574a7f146253206522b4653ef7
Victor Ingvarsson <victor.ingvarsson@gmail.com>
2022-08-29 17:30:52 +0200

    feat(main): updated readme and parser
    Parser now accepts P, T, A, F and parses into XML

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python3-lines-to-XML
 Converting data strings into XML file
+
+Requires Python3 >= 9
--- lines-to-XML.py
+++ lines-to-XML.py
@@ -1,34 +1,65 @@
 import xml.etree.ElementTree as ET
 
 def main(): 
-    input_string = "P|Carl Gustaf|Bernadotte\nT|0768-101801|08-101801"
+    input_file = open("input.txt", "r")
+    input_string = input_file.read()
     file_name = "output.xml"
-
     formatted_string = input_string.splitlines()
-    # list_items = []
     xml = ET.Element("people")
-    person = ET.SubElement(xml, "person")
+    family_state = False
 
     for items in formatted_string: 
         sep_string = items.split(sep = "|")
-        if sep_string[0] == "P": 
-           firstname = ET.SubElement(person, "firstname")
-           firstname.text = sep_string[1]
-           lastname = ET.SubElement(person, "lastname")
-           lastname.text = sep_string[2]
+
+        if sep_string[0] == "P":
+            family_state = False
+            person = ET.SubElement(xml, "person") 
+            firstname = ET.SubElement(person, "firstname")
+            firstname.text = sep_string[1]
+            lastname = ET.SubElement(person, "lastname")
+            lastname.text = sep_string[2]
+
         elif sep_string[0] == "T":
-            phone = ET.SubElement(person, "phone")
+            if family_state == True: 
+                phone = ET.SubElement(family, "phone")
+            
+            if family_state == False: 
+                phone = ET.SubElement(person, "phone")
+           
             mobile = ET.SubElement(phone, "mobile")
             mobile.text = sep_string[1]
             telephone = ET.SubElement(phone, "telephone")
             telephone.text = sep_string[2]
 
-        # list_items.append(sep_string)
-        # formatted_list = list(list_items)
+        elif sep_string[0] == "A": 
+            if family_state == True: 
+                address = ET.SubElement(family, "address")
 
-    
+            if family_state == False: 
+                address = ET.SubElement(person, "address")
+
+            street = ET.SubElement(address, "street")
+            street.text = sep_string[1]
+            city = ET.SubElement(address, "city")
+            city.text = sep_string[2]
+            
+            if len(sep_string) > 3: 
+                postal_number = ET.SubElement(address, "postalnumber")
+                postal_number.text = sep_string[3]
+
+        elif sep_string[0] == "F":
+            family_state = True
+            family = ET.SubElement(person, "family")
+            name = ET.SubElement(family, "name")
+            name.text = sep_string[1]
+            born = ET.SubElement(family, "born")
+            born.text = sep_string[2]
+
+# builds xmlTree from element
     xmlTree = ET.ElementTree(element=xml)
+# indent properly
     ET.indent(xml, space="\t", level=0)
+# write to file
     xmlTree.write(file_name, encoding="utf-8")
 
 if __name__ == "__main__":
